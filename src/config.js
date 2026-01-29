861b8f549ef4948f434cd7be6804e664dae4454b
Kero Codes <onemove1112@gmail.com>
2026-01-29 19:14:07 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,9 +5,10 @@ const LOCAL_IP = "192.168.18.27";
 
 // 2. YOUR REAL LIVE BACKEND URL (From Render, Railway, etc.)
 // Replace 'likhat-habi-api' with your actual service name
-const PROD_URL = "https://likhat-habi-api.onrender.com"; 
+const PROD_URL = "https://likha-t-habi.onrender.com"; 
 
 // 3. Automatically switch based on where the app is running
 export const API_URL = window.location.hostname === "localhost" 
     ? `http://${LOCAL_IP}:5000` 
     : PROD_URL;
+
